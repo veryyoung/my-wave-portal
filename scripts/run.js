012dea8ec5ac653335a53b5408b12d688da351f1
veryyoung <veryyoung.me@gmail.com>
2022-03-09 22:33:59 +0800

    👀 Writing a script to deploy locally

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,33 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount = await waveContract.getTotalWaves();
+
+    let waveTx = await waveContract.wave();
+    await waveTx.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTx = await waveContract.connect(randomPerson).wave();
+    await waveTx.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
     try {
         await main();
-        process.exit(0); // exit Node process without error
+        process.exit(0);
     } catch (error) {
         console.log(error);
-        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+        process.exit(1);
     }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
 };
 
 runMain();
