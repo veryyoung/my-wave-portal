c381eb92359fa97bbfdc1925a15dfab4a1bba8c5
veryyoung <veryyoung.me@gmail.com>
2022-03-11 12:21:12 +0800

    Connect with rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+dotenv.config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const mnemonic = process.env.MNEMONIC.trim();
+const alchemyKey = process.env.ALCHEMY_KEY;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyKey}`,
+      accounts: {
+        mnemonic
+      }
+    }
+  }
 };
